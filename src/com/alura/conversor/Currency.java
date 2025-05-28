149275897ba37e21fe27c1864ee424a16a9a18f7
JPMB91 <juan.maldonado@live.cl>
2025-05-28 10:32:30 -0400

    refactor:(Currency): change from record to class

--- src/com/alura/conversor/Currency.java
+++ src/com/alura/conversor/Currency.java
@@ -1,4 +1,44 @@
 package com.alura.conversor;
 
-public record Currency(String base_code,String target_code, double conversion_result) {
+public class Currency {
+    private String base_code;
+    private String target_code;
+    private double conversion_result;
+    private int amount;
+
+
+    public Currency(String base_code, String target_code, int amount) {
+        this.base_code = base_code;
+        this.target_code = target_code;
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getBase_code() {
+        return base_code;
+    }
+
+    public String getTarget_code() {
+        return target_code;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                El valor %d [%s] corresponde al valor final de => %.2f %s
+               """
+                .formatted(
+                        this.amount,
+                        this.base_code,
+                        this.conversion_result,
+                        this.target_code
+                );
+    }
 }
